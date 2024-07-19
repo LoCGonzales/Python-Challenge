9253a7d2c0ba8c52303e8c35640bc60c9cfa16ad
Logan Chancellor-Gonzales <lnc.gonzales2@gmail.com>
2024-07-18 19:27:59 -0500

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,18 +2,21 @@ print("Financial Analysis")
 print("-----------------")
 
 import os
+import csv
 import pandas as pd
 
 budget_csv = os.path.join('PyBank', 'Resources', 'budget_data.csv')
+
 df = pd.read_csv(budget_csv)
 
 number_of_months = len(df)
 print(f"Total months: {number_of_months}")
 
 total_amount = df["Profit/Losses"].sum()
-print(f"Total: ${total_amount}")
+print(f"Total: ${total_amount:,.0f}")
 
 df['Change'] = df['Profit/Losses'].diff()
+
 average_change = df['Change'].mean()
 print(f"Average Change: ${average_change:.2f}")
 
